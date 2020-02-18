dfb8cee25a1029cd18518e203838bb84dd85a6a4
Tueloper <ozurumbatochukwu@yahoo.com>
2020-02-18 12:44:13 +0100

    Authenication with OAuth Functional

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import passport from 'passport';
+import { sendErrorResponse, sendSuccessResponse } from './../util/sendResponse';
 const router = express.Router();
 
 router.get(
@@ -12,6 +13,17 @@ router.get(
 	})
 );
 
-router.get('/auth/google/callback', passport.authenticate('google'));
+router.get('/auth/google/callback', passport.authenticate('google'), (req, res) => {
+	res.send({ message: 'Log In successfull' });
+});
+
+router.get('/api/current/user', (req, res) => {
+	res.send(req.user);
+});
+
+router.get('/api/logout', (req, res) => {
+	req.logout();
+	res.send(req.user);
+});
 
 export default router;
