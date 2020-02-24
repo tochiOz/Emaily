f591a715c6d918d1d5c4c403155a4717ebd7a045
Tueloper <ozurumbatochukwu@yahoo.com>
2020-02-24 03:02:48 +0100

    Proxy config bug fixed

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,5 +1,10 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = (app) => {
-	app.use(proxy.createProxyMiddleware('/auth/google', { target: 'http://localhost:7000' }));
+	app.use(
+		proxy.createProxyMiddleware('/auth/google', {
+			target: 'http://localhost:7000',
+			changeOrigin: true
+		})
+	);
 };
