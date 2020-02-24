cb2f2beb1db16865029ee97e3c5ab7fd41fafa9f
Tueloper <ozurumbatochukwu@yahoo.com>
2020-02-24 12:42:33 +0100

    redux setup in process

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './../App.css';
+
+function App (){
+	return (
+		<div className='App'>
+			<div className='App-header'>
+				<h1>Hi There</h1>
+			</div>
+		</div>
+	);
+}
+
+export default App;
