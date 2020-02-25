3a23b6f7b5cb8e525e9dc0998794acd4ad9a2e3b
Tueloper <ozurumbatochukwu@yahoo.com>
2020-02-25 15:13:05 +0100

    setup billing

--- src/webpack/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	PORT: 7000,
-	GoogleClientID: '907094750985-q088i8tn0ru5prho5h33s2k9h5paojlu.apps.googleusercontent.com',
-	GoogleClientSECRET: 'x9vvz24pJpGuNYRQdiL2Kxjn',
-	cookieSecret: 'cnianccvnoiojcnoweycwundaw89765ewfdw8e9dj9cai'
-};
--- src/webpack/prod.js
+++ src/webpack/prod.js
@@ -2,5 +2,7 @@ module.exports = {
 	PORT: process.env.PORT,
 	GoogleClientID: process.env.GoogleClientID,
 	GoogleClientSECRET: process.env.GoogleClientSECRET,
-	cookieSecret: process.env.cookieSecret
+	cookieSecret: process.env.cookieSecret,
+	stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+	stripeSecretKey: process.env.STRIPE_SECRET_KEY
 };
