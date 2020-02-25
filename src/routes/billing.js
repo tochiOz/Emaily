7da098b282ee496d1788119eb0eede569f0901de
Tueloper <ozurumbatochukwu@yahoo.com>
2020-02-25 18:17:05 +0100

    Middleware added

--- src/routes/billing.js
+++ src/routes/billing.js
@@ -1,11 +1,10 @@
 import express from 'express';
 import keys from './../webpack/keys';
 const stripe = require('stripe')(keys.stripeSecretKey);
-const models = require('./../models');
 const router = express.Router();
-const { User } = models;
+import requireLogin from './../middlewares/requireLogin';
 
-router.post('/api/stripe/credits', async (req, res) => {
+router.post('/api/stripe/credits', requireLogin, async (req, res) => {
 	//CREATE STRIPE CHARGE
 	const charge = await stripe.charges.create({
 		amount: '500',
