629a3e997549f647d8575897e8deafa6ac8cb56a
Tueloper <ozurumbatochukwu@yahoo.com>
2020-02-27 15:29:43 +0100

    form review in process

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -69,6 +69,7 @@ class Header extends Component {
 
 //connecting header component to the store
 function mapStateToProps ({ auth }){
+	// console.log(auth);
 	return { auth };
 }
 
--- client/src/components/surveys/surveyForm.js
+++ client/src/components/surveys/surveyForm.js
@@ -15,7 +15,7 @@ class SurveyForm extends Component {
 	render () {
 		return (
 			<div>
-				<form onSubmit={this.props.handleSubmit((values) => console.log(values))}>
+				<form onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}>
 					{this.renderFields()}
 					<Link to='/surveys' className='red left btn-flat white-text'>
 						<i className='material-icons right'>arrow_back</i>
@@ -48,7 +48,9 @@ function validate (values){
 	return errors;
 }
 
+// redux form helper helps to make te form possible and persistent
 export default reduxForm({
 	validate,
-	form: 'surveyForm'
+	form: 'surveyForm',
+	destroyOnUnmount: false
 })(SurveyForm);
